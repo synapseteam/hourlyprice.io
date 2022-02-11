9a2409427b90f40e1d19e6b0964a913d8cf38d2a
yarKode <grigoriy.yarko@grupa.itdesk.eu>
2022-02-11 13:15:06 +0200

    ESlint config fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,9 @@
 module.exports = {
-  settings: {
-    "import/resolver": {
-      node: {
-        extensions: [".js", ".jsx"],
-      },
-    },
-  },
+  parser: "@babel/eslint-parser",
   env: {
     browser: true,
-    node: true,
     es2021: true,
+    node: true,
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
