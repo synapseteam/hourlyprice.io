5f42b759d17f89bcdd516e9cd8d55da64ca4db11
Vladyslav Katerusha <vladyslavkaterusha@synapseteam.com>
2022-05-11 13:14:21 +0300

    fix index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import "./index.scss";
 
 const container = document.getElementById("root");
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
 root.render(
   <BrowserRouter>
